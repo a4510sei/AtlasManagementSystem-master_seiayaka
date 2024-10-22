c8227a1b0ca3248841e028d4c35fece43bc9ad56
a4510sei <asei4510you@gmail.com>
2024-10-22 23:43:35 +0900

    あ

---
+++ database/migrations/2024_10_22_142947_change_post_sub_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePostSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('post_sub_categories', function (Blueprint $table){
+            $table->integer('post_id')->index()->references('id')->on('post')->onDelete('cascade')->comment('投稿のid')->change(); ;
+            $table->integer('sub_category_id')->index()->references('id')->on('sub_categories')->onDelete('cascade')->comment('サブカテゴリーid')->change(); ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('post_sub_categories');
+    }
+}
